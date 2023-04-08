fa6f074dd9639887959682ffa677de93253680f6
Pattles <doomrock8@gmail.com>
2023-04-08 15:53:03 -0400

    moved setup to events.py

--- cogs/management.py
+++ cogs/management.py
@@ -37,57 +37,6 @@ class Management(commands.Cog):
         await asyncio.sleep(2)
         await msg.delete()
 
-    @commands.hybrid_command(description='Run this command to setup the bot. If you don\'t, the bot will not function properly.')
-    async def setup(self, ctx):
-        embed = discord.Embed(description='Working...', color=self.bot.color)
-        msg = await ctx.send(embed=embed)
-
-        """Opening up all jsons."""
-        with open(LIKES_JSON, 'r') as infile:
-            likes_json = json.load(infile)
-        with open(LEVELING_JSON, 'r') as infile:
-            leveling_json = json.load(infile)
-        with open(GM_JSON, 'r') as infile:
-            gm_json = json.load(infile)
-
-        """Checking if the bot already has the guild's data."""
-        if str(ctx.guild.id) in likes_json and str(ctx.guild.id) in leveling_json and str(ctx.guild.id) in gm_json:
-            embed = discord.Embed(description=f'It looks like the bot has already been setup by someone in **{ctx.guild.name}**.', color=self.bot.color)
-            await msg.edit(embed=embed)
-            return
-        
-        """If not, moves on to the rest of the command."""
-
-        """Setting up the 'likes' module."""
-        if str(ctx.guild.id) not in likes_json:
-            likes_json[str(ctx.guild.id)] = {}
-            likes_json[str(ctx.guild.id)][str(ctx.author.id)] = {}
-            likes_json[str(ctx.guild.id)][str(ctx.author.id)]['likes'] = 0
-
-            with open(LIKES_JSON, 'w') as f:
-                json.dump(likes_json, f)
-
-        """Settings up the 'leveling' module."""
-        if str(ctx.guild.id) not in leveling_json:
-            leveling_json[str(ctx.guild.id)] = {}
-            leveling_json[str(ctx.guild.id)][str(ctx.author.id)] = {}
-            leveling_json[str(ctx.guild.id)][str(ctx.author.id)]['experience'] = 0
-            leveling_json[str(ctx.guild.id)][str(ctx.author.id)]['level'] = 0
-
-            with open(LEVELING_JSON, 'w') as f:
-                json.dump(leveling_json, f)
-
-        """Setting up the 'gm_tracker' module."""
-        if str(ctx.guild.id) not in gm_json:
-            gm_json[str(ctx.guild.id)] = {}
-            gm_json[str(ctx.guild.id)][str(ctx.author.id)] = {}
-            gm_json[str(ctx.guild.id)][str(ctx.author.id)]['good_mornings'] = 0
-
-            with open(GM_JSON, 'w') as f:
-                json.dump(gm_json, f)
-
-        embed = discord.Embed(description=f'Successfully setup the bot for **{ctx.guild.name}**. Have fun!', color=self.bot.color)
-        await msg.edit(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Management(bot))
