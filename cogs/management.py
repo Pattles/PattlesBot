c4553618879bbb9092f4bee08e6c3df81fc79842
Pattles <doomrock8@gmail.com>
2023-04-09 21:39:58 -0400

    added keywordban

--- cogs/management.py
+++ cogs/management.py
@@ -37,6 +37,33 @@ class Management(commands.Cog):
         await asyncio.sleep(2)
         await msg.delete()
 
+    @commands.hybrid_command(description='Bans every member in the server with a keyword in their username. Case sensitive!')
+    @commands.has_permissions(administrator=True)
+    async def keywordban(self, ctx, keyword:str):
+        embed = discord.Embed(description='Working...', color=self.bot.color)
+        msg = await ctx.send(embed=embed)
+        
+        banned_users, not_banned_users = [], []
+        
+        for member in ctx.guild.members:
+            if keyword in member.name:
+                try:
+                    await ctx.guild.ban(member)
+                    banned_users.append(member)
+                except:
+                    not_banned_users.append(member)
+
+        embed = discord.Embed(title=f'Banned {len(banned_users)} {"members" if len(banned_users) != 1 else "member"}.', color=self.bot.color)
+        
+        if not_banned_users != []:
+            embed.add_field(name=f'{len(not_banned_users)} {"members" if len(not_banned_users) != 1 else "member"} fit the paramenters but could not be banned', value=' '.join(member.mention for member in not_banned_users[0:30]))
+
+        try:
+            await msg.edit(embed=embed)
+        except:
+            await ctx.send(embed=embed)
+
+
 
 async def setup(bot):
     await bot.add_cog(Management(bot))
